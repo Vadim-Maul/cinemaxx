9c1f0b86d92214b5aa84e8b58b7ab2fe185b10e8
sw-00-kmit1 (Maul <sw-00-kmit1@schulung-bbw.de>
2021-11-23 10:08:09 +0100

    carousel

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@ $(document).ready(function () {
    $('.carousel__inner').slick({
       infinite: true,
       rt1: true,
-      dots: true,
+      dots: false,
       speed: 1500,
       slideToShow: 1,
       adaptiveHeight: false,
       autoplay: true,
-      autoplaySpeed: 3000
+      autoplaySpeed: 3000,
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/chevron_left_solid.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/chevron_right_solid.png"></button>'
    });
 });
