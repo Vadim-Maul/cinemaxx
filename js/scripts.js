6eab228af64d080e69824bee0da344349b6a7f99
sw-00-kmit1 (Maul <sw-00-kmit1@schulung-bbw.de>
2021-11-23 09:20:28 +0100

    cinemaxx.part1

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+   $('.carousel__inner').slick({
+      infinite: true,
+      rt1: true,
+      dots: true,
+      speed: 1500,
+      slideToShow: 1,
+      adaptiveHeight: false,
+      autoplay: true,
+      autoplaySpeed: 3000
+   });
+});
