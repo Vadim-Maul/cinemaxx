a740b8611988c9f7f1bf350fe7cb734d62242846
sw-00-kmit1 (Maul <sw-00-kmit1@schulung-bbw.de>
2021-11-23 10:23:51 +0100

    cinemaxx_part2

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,11 @@ $(document).ready(function () {
       infinite: true,
       rt1: true,
       dots: false,
-      speed: 1500,
+      speed: 2000,
       slideToShow: 1,
       adaptiveHeight: false,
       autoplay: true,
-      autoplaySpeed: 3000,
+      autoplaySpeed: 4000,
       prevArrow: '<button type="button" class="slick-prev"><img src="img/chevron_left_solid.png"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="img/chevron_right_solid.png"></button>'
    });
